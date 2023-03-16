39c0d50f6e401f175258519ea8d46316abfe18b7
AlexanderPelevin <anpelevin@gmail.com>
2023-03-16 11:35:03 +0300

    created data_out

--- main.py
+++ main.py
@@ -11,3 +11,10 @@ addr = ("77.88.55.242", 443)
 # AF_INET = IPv4
 ya_sock.connect(addr)
 # addr = (ip адрес, port)
+
+data_out = b"GET / HTTP/1.1\r\nHost:ya.ru\r\n\r\n"
+# b - двоичный вид
+ya_sock.send(data_out)
+
+# \r\n - перевод каретки
+# \r\n\r\n - в конце два раза перевод каретки
